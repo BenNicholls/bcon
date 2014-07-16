4bb61731b61e4e7de19e4419ef5b44d6ea8059cd
Benjamin Nicholls <nicholls.ben@gmail.com>
2014-07-16 16:11:20 +0000

    custom error, help, cleanup

--- bcon.go
+++ bcon.go
@@ -11,24 +11,29 @@ var listFile *os.File
 func main() {
 
 	flag.Parse()
-	printArgs()
 
 	//process verbs (add, remove, etc)
 	switch flag.Arg(0) {
 	case "add":
 		//add command: takes a path to a file, a name and a tag (optional).
-		fmt.Println("adding", flag.Arg(1))
-		addEntry()
+		err := addEntry()
+		if err != nil {
+			fmt.Println(err)
+		}
 	case "help":
-		fmt.Println("This is the help for bcon! TODO: help people.")
+		printHelp()
 	default:
 		fmt.Println("Not a valid command, try bcon help.")
 
 	}
 }
 
-func addEntry() {
+func addEntry() error {
 
+	//check args. needs to be filename, list name, then optionally, a list of tags
+	// var fileName = flag.Args(1)
+	// var entryName = flag.Args(2)
+	// var tags = flag.Args(3)
 	var err error
 
 	//open file list for writing (in append mode), if fail, create new file
@@ -42,9 +47,11 @@ func addEntry() {
 	if flag.Arg(1) != "" {
 		listFile.WriteString(flag.Arg(1) + " ")
 	} else {
-		listFile.WriteString("stuff\n")
+		return BconError{"No argument to add!"}
 	}
 
+	//all good, lets boogie.
+	return nil
 }
 
 func checkError(e error) {
@@ -53,8 +60,19 @@ func checkError(e error) {
 		panic(e)
 	}
 }
-func printArgs() {
-	for i := 0; i < flag.NArg(); i++ {
-		fmt.Println(i, ":", flag.Arg(i))
-	}
+
+func printHelp() {
+	fmt.Println("bcon commands:\n")
+	fmt.Println("   add (filename, name, [tags])  Adds a file.")
+	fmt.Println("   search (name or tag)          Search the filelist by name or tag.")
+	fmt.Println("   remove (name or tag)          Remove a file from the file list.")
+	fmt.Println("   help                          Show this text. ")
+}
+
+type BconError struct {
+	what string
+}
+
+func (e BconError) Error() string {
+	return e.what
 }
