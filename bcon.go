45dc4c703ce6fc0fadc3cf7debb9a03b0e15c071
Benjamin Nicholls <nicholls.ben@gmail.com>
2014-08-05 22:55:32 +0000

    creates directory properly, cleanup

--- bcon.go
+++ bcon.go
@@ -44,6 +44,8 @@ func main() {
 		entrylist.Print()
 	case "help":
 		printHelp()
+	case "":
+		fmt.Println("Try \"bcon help\" maybe!")
 
 	//No command, attempt to launch!
 	default:
--- entries/io.go
+++ entries/io.go
@@ -1,21 +1,29 @@
 package entries
 
 import "os"
+import "path"
 import "bufio"
 import "strings"
 
 //Parse the file list. If there is no filelist, it makes a blank one.
 //TODO: actually throw some errors
-func ParseFilelist(path string) (BconEntrylist, error) {
+func ParseFilelist(filePath string) (BconEntrylist, error) {
 
 	//entries NOTE: is 50 too much as a default capacity? too small? who can say
 	list := BconEntrylist{make([]BconEntry, 0, 50)}
 
-	listFile, err := os.OpenFile(path, os.O_RDWR|os.O_APPEND, 0660)
+	listFile, err := os.OpenFile(filePath, os.O_RDWR|os.O_APPEND, 0660)
 	if err != nil {
 		//NOTE: this just assumes the error indicates that the file doesn't
 		//exist. Really, this should be checked I guess.
-		listFile, err = os.Create(path)
+
+		//if the directory doesn't exist, create it.
+		err = os.MkdirAll(path.Dir(filePath), 0700)
+		if err != nil {
+			return list, err
+		}
+
+		listFile, err = os.Create(filePath)
 		if err != nil {
 			return list, err
 		}
